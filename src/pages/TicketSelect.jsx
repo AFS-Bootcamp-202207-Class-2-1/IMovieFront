55611572d5737864c44beef827ff144fad0b6f36
JARRELL CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENJA30@OOCL.COM>
2022-08-10 08:44:23 +0800

    fix: TicketSelect and SelectSeat

--- src/pages/TicketSelect.jsx
+++ src/pages/TicketSelect.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../assets/less/ticketSession.less'
 import { useState,useEffect } from 'react';
-import { getSessions,getMovieDetail } from '../api/ticketSelect'
+import { getSessions,getMovieDetail,getSessionSeats } from '../api/ticketSelect'
 import { Button,DatePicker } from "antd";
 import SelectSeat from '../components/SelectSeat';
 import { useParams } from 'react-router-dom'
@@ -9,6 +9,7 @@ import { useParams } from 'react-router-dom'
 const TicketSelect = () => {
 
     const [sessionList,setSessionList] = useState([])
+    const [sessionSeats,setSessionSeats] = useState({})
     const [details,setDetails] = useState({})
     const { id } = useParams();
 
@@ -26,35 +27,35 @@ const TicketSelect = () => {
     };
 
     const selectSession = (id) => {
-        getSessions(id).then((response) => {
+        getSessionSeats(id).then((response) => {
             console.log(response.data)
-            setSessionList(response.data)
+            setSessionSeats(response.data)
         })
     }
 
     return (
          <div>
             <div className="Detail-Movie">
-            <img className='movie-cover' src={details.movieImage} alt="cover" />
-            <div className="DetailIntroduce">
-                <div className="fs-20 black-1 movie-name">{details.movieName}</div>
-                <div className="fs-18 black-1 mt-28">{details.movieCatagory}</div>
-                <div className="fs-14 black-1 mt-20">时长： {details.movieTime} 分钟</div>
-                <div className="fs-14 black-1 mt-20">主演： {details.moviePerformers}</div>
-                <div className="fs-14 black-1 mt-20 session-container">场次：<DatePicker onChange={onChange} />
-                    <div className='session-selector'>
-                        {
-                            sessionList.map((item,index) => (
-                                <Button key={index} value={item} onClick={()=>{ selectSession(item.cinemaMovieTimeId) }}>{item.cinemaName} {item.cinemaMovieTimeWatchtime} {item.cinemaMovieTimeEndtime}</Button>
-                        ))}
+                <img className='movie-cover' src={details.movieImage} alt="cover" />
+                <div className="DetailIntroduce">
+                    <div className="fs-20 black-1 movie-name">{details.movieName}</div>
+                    <div className="fs-18 black-1 mt-28">{details.movieCatagory}</div>
+                    <div className="fs-14 black-1 mt-20">时长： {details.movieTime} 分钟</div>
+                    <div className="fs-14 black-1 mt-20">主演： {details.moviePerformers}</div>
+                    <div className="fs-14 black-1 mt-20 session-container">场次：<DatePicker onChange={onChange} />
+                        <div className='session-selector'>
+                            {
+                                sessionList.map((item,index) => (
+                                    <Button key={index} value={item} onClick={()=>{ selectSession(item.cinemaMovieTimeId) }}>{item.cinemaName} {item.cinemaMovieTimeWatchtime} {item.cinemaMovieTimeEndtime}</Button>
+                            ))}
+                        </div>
                     </div>
                 </div>
+                <div className='score'>
+                    {details.movieScore}
+                </div>
             </div>
-            <div className='score'>
-                {details.movieScore}
-            </div>
-         </div>
-         <SelectSeat />
+         <SelectSeat sessionSeats={sessionSeats}/>
         </div>
     );
 };
